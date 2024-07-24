5e6b8154e52d283744310f3d607f94d3da8af8a0
adfar-136 <adfarrasheed136@gmail.com>
2024-07-24 22:51:52 +0530

    react code started

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import About from "./components/About"
+import Blog from "./components/Blog";
+import Contact from "./components/Contact"
+function App(){
+  var visible = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+     <h1>React App</h1>
+     {visible ? <About/>:<Contact/>}
+     {visible && <Blog/>}
+    </>
+  )
 }
 
-export default App;
+// export {App}
+export default App
---
+++ src/components/About.js
@@ -0,0 +1,18 @@
+function About(){
+    // const myStyle = {
+    //     color:"red",
+    //     backgroundColor:"yellow",
+    //     fontSize:"30px",
+    //     border: "2px solid red"
+    // }
+    return (
+        <>
+         <h1 style={ {color:"red",
+        backgroundColor:"yellow",
+        fontSize:"30px",
+        border: "2px solid red"}}>About</h1>
+         <p>Some text about who we are and what we do.</p>
+        </>
+    )
+}
+export default About
