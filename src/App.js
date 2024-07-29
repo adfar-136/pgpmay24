97a7cc649ed6eb433609f699d0ff40e4011c40a7
adfar-136 <adfarrasheed136@gmail.com>
2024-07-29 20:30:51 +0530

    batching, controlled components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { useState } from 'react'
 
 export default function App() {
-  const [condition,setCondition] = useState(false)
-  function handleBackground(){
-    setCondition(!condition)
+  const [name,setName] = useState("")
+  const [conditin,setcondition] = useState(false)
+  function handleSubmit(e){
+    e.preventDefault()
+    setcondition(true)
   }
   return (
-    <div style={{backgroundColor:condition?"black":"white",color:condition?"white":"black"}}>
-      <h1>Hello, world!</h1>
-      <p>This is a React app.</p>
-      <button onClick={handleBackground}>{condition?"Light":"Dark"} Mode</button>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="text" 
+        value={name} onChange={(e)=>setName(e.target.value)}/>
+        <button type="submit">Submit</button>
+      </form>
+      {conditin && <h1>{name}</h1>}
     </div>
   )
 }
